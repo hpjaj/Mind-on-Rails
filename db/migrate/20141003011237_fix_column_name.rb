f81ce9e9dd80cf751f97005ddc439e3074281520
hpjaj <hmlevine@gmail.com>
2014-10-03 10:04:53 -0600

    Fixed database issues with test and dev and production

--- db/migrate/20141003011237_fix_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def change
-    rename_column :stacks, :body, :description
-  end
-end
--- db/migrate/20141003012848_drop_stacks.rb
+++
@@ -1,5 +0,0 @@
-class DropStacks < ActiveRecord::Migration
-  def change
-    drop_table :stacks
-  end
-end
