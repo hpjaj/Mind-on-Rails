074e7f493533d1a2bdec6589a60fce701c6e676d
hpjaj <hmlevine@gmail.com>
2014-10-06 12:08:09 -0600

    Stack_spec for the stacks model is passing

--- spec/models/stack_spec.rb
+++ spec/models/stack_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Stack, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "associations" do
+
+    it "has many and belongs to notes" do
+      expect(Stack.new).to have_and_belong_to_many(:notes)
+    end
+
+  end
+  
 end
