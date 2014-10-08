aa0816baaf7cd10f86c2ed47348a9286b424002d
hpjaj <hmlevine@gmail.com>
2014-10-08 00:04:52 -0600

    Added validation to notes.rb for body and title length, and stack presence

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,9 @@ class Note < ActiveRecord::Base
   has_and_belongs_to_many :stacks
 
   scope :recently_updated_first, lambda { order("notes.updated_at DESC")}
+
+  validates :title, length: { minimum: 5, too_short: "Minimum length of 5 characters" }
+  validates :body, length: { minimum: 15, too_short: "Minimum length of 15 characters" }
+  validates :stacks, :presence => { :message => "Choose at least 1 stack" }
+
 end
