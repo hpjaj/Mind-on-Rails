742235c063b2cb1e494ab1fec5d2c53b80418834
hpjaj <hmlevine@gmail.com>
2014-10-08 20:31:36 -0600

    Created spec for testing a single stack's view It tests to ensure that the stacks index page contains a link for a given stack.  And that when you click on that link, it brings you to that stack's single stack view.  And that single stack view contains only the notes associated with that stack, and does not contain any notes that do note have that stack associated with them.

---
+++ spec/features/single_stack_view_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'A single stack view' do
+
+  before do
+    stack = create(:stack)
+    stack_2 = create(:stack, :title => "CSS Styling", :description => "This is all about the look and feel", id: 2)
+    note = create(:note)
+    note_2 = create(:note, :title => "This is the second note", stack_ids: [1])
+    note_3 = create(:note, :title => "This is the third note", stack_ids: [2])
+    visit stacks_path
+  end
+
+  it "Contains all notes with that Stack's ID" do
+    click_link 'Tricks'
+    expect( page ).to_not have_content("CSS Styling")
+    expect( page ).to_not have_content("This is the third note")
+    expect( page ).to have_content("Tricks")
+    expect( page ).to have_content("This is a note")
+    expect( page ).to have_content("This is the second note")
+    expect( page ).to have_content("This stack is for tricks in Ruby on Rails")
+  end
+end
