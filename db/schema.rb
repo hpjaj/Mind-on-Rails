5e068d65235b5ca918f4592409304070824e07a0
hpjaj <hmlevine@gmail.com>
2014-10-13 17:26:54 -0600

    Created public column in notes. Added public checkbox to new note form.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013165213) do
+ActiveRecord::Schema.define(version: 20141013223606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141013165213) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.boolean  "public",     default: false
   end
 
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
