c21717cc0d2373629a10ac567784563b65b0b66d
hpjaj <hmlevine@gmail.com>
2014-10-14 12:56:35 -0600

    Restyled the alert boxes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'faker'
 
+User.destroy_all
+Stack.destroy_all
+Note.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
