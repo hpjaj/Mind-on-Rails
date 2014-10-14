404d56e7696b70fafff06c32e8a5ad15c4d4b637
hpjaj <hmlevine@gmail.com>
2014-10-14 11:10:00 -0600

    Fixed policy error and heroku deploy error. Styled alert boxes.

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,7 +1,12 @@
 class NotePolicy < ApplicationPolicy
   
   def create?
+    puts "#{user.present?} #{record.user} #{user}"
     user.present? && (record.user == user || user.admin?)
+  end
+
+  def new?
+    user.present?
   end
 
   def update?
