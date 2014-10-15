dcd287ecfa97508e48da5660c2f7dc9822468388
hpjaj <hmlevine@gmail.com>
2014-10-15 15:48:29 -0600

    Remove excess user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,12 @@
 Rails.application.routes.draw do
-  
-  # get 'users/index'
 
-  # get 'users/show'
-
-  resources :users, only: [:update, :index]
+  resources :users, only: [:update]
   
   get 'search' => 'notes#search'
-
 
   resources :notes, :stacks
   
   devise_for :users
-
-  # get 'notes/index'
-
-  # get 'notes/new'
-
-  # get 'notes/show'
-
-  # get 'stacks/index'
-
-  # get 'stacks/new'
-
-  # get 'stacks/edit'
-
-  # get 'stacks/show'
-
-  # get 'welcome/index'
-
 
   get 'about' => 'welcome#about'
 
