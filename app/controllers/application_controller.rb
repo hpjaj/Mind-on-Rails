ae4fa10aa5037766b9aa1d1a9e5f6975687dd0b7
hpjaj <hmlevine@gmail.com>
2014-10-17 09:03:00 -0600

    Modified application controller so that non authorized pundit actions are rescued and redirected to root with a stock alert message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-    redirect_to root_url, alert: exception.message
-  end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def after_sign_in_path_for(resource)
     stacks_path
@@ -19,4 +17,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
