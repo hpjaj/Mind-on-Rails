9b97e62811fad4bf4339e49c95894c2b83fdb22d
hpjaj <hmlevine@gmail.com>
2014-10-29 17:06:25 -0600

    Created framework for a flashcard spec file

---
+++ spec/features/user_creates_and_views_flashcard_notes_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "flashcards" do
+
+  it "cannot be created by an anonymous user" do
+  end
+
+  context "for a logged in standard user" do
+    it "can make their note a flashcard" do
+    end
+
+    it "can make another user's public note a flashcard of theirs" do
+    end
+
+    it "can view all their flashcards in one view" do
+    end
+
+    it "cannot view another users flashcards" do
+    end
+    
+  end
+
+
+end
