053aae194a0d5a384d883385baf69f5d7880076b
hpjaj <hmlevine@gmail.com>
2016-02-14 09:08:22 -0700

    Removes HTTPS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -81,6 +81,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'mind-on-rails.herokuapp.com' }  
+  config.action_mailer.default_url_options = { host: 'mind-on-rails.herokuapp.com' }
 
 end
