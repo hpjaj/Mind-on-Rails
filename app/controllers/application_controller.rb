c46b122646007c67ee1985be7a78e8d923f4e96e
Harry Levine <hmlevine@gmail.com>
2017-12-17 13:20:18 -0700

    Uses Devise's strong parameters config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-  
   include Pundit
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -16,8 +15,12 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # Uses Devise's strong parameters config
+  #
+  # @see https://github.com/plataformatec/devise#strong-parameters
+  #
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :username]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
   end
 
   private
