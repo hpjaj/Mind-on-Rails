945db2e253732099dcb804021f19cfbec26f1f95
Harry Levine <hmlevine@gmail.com>
2017-12-24 20:22:34 -0700

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,24 @@
 Rails.application.routes.draw do
-
-  resources :users, only: [:update]
-  
-  get 'search' => 'notes#search'
-
-  get 'flashcards' => 'flashcards#index'
-  
-  get 'trending' => 'notes#trending'
-
-  resources :stacks
+  devise_for :users
 
   resources :notes do
     resources :flashcards, only: [:create, :destroy]
   end
-  
-  devise_for :users
 
-  post 'notes/:note_id/up-vote' => 'votes#up_vote', as: :note_up_vote
-  post 'notes/:note_id/down-vote' => 'votes#down_vote', as: :note_down_vote
+  resources :stacks
+  resources :users, only: [:update]
 
-  get 'about' => 'welcome#about'
-  
+  get 'flashcards' => 'flashcards#index'
+
+  get 'search' => 'notes#search'
+  get 'trending' => 'notes#trending'
+
   get 'admin/users' => 'users#index'
   get 'users/:id', to: 'users#show', as: :userShowPage
 
-  root 'welcome#index'
+  post 'notes/:note_id/down-vote' => 'votes#down_vote', as: :note_down_vote
+  post 'notes/:note_id/up-vote' => 'votes#up_vote', as: :note_up_vote
 
+  get 'about' => 'welcome#about'
+  root 'welcome#index'
 end
